f874711a8f7ca82a28dfe1c813d1d38173408274
Siddharth Sahay <siddharthsahay2004@gmail.com>
2026-08-06 19:50:05 +0530

    new dataset added

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,8 +1,10 @@
 from .synthetic import SyntheticNeedleDataset, SyntheticPriorityDataset
 from .real_squad import SQuADDatasetLoader
+from .real_wikitext import WikiTextDatasetLoader
 
 __all__ = [
     "SyntheticNeedleDataset",
     "SyntheticPriorityDataset",
     "SQuADDatasetLoader",
+    "WikiTextDatasetLoader",
 ]
